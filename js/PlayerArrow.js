7d2ec6357593e331f948e6d2264e0d17bc6f0d35
nandindugrau <111690494+nandindugrau@users.noreply.github.com>
2022-08-21 10:17:04 -0300

    Add files via upload

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -55,31 +55,10 @@ class PlayerArrow {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
-    
-    /*if (this.body.velocity.x > 0 || this.body.position.x > 400) {
-      var position = [this.body.position.x, this.body.position.y];
-      trajectory.push(position);
-    }*/
-
-    /*if (this.body.velocity.x < 0 && this.body.position.x < 400) {
-      var position = [this.body.position.x, this.body.position.y];
-      this.trajectory(position);
-    }*/
-
-    /*if (this.body.velocity.x > 0 this.body.position.x > 400) {
-      var position = [this.body.position.x, this.body.position.y];
-      this.trajectory.push();
-    }*/
 
     for (var i = 0; i < this.trajectory.length; i++) {
       fill("white");
-
-      //ellipse(this.trajectory[0], this.trajectory[1], 5, 5);
-      //ellipse(trajectory[i][0], trajectory[i][1], 5, 5);
       ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5);
-      //ellipse(this.trajectory(i)(0), this.trajectory(i)(1), 5, 5);
-      
     }
-    
   }
 }
